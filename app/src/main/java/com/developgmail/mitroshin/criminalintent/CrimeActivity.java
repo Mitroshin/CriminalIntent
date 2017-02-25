4a8574c1bf70d864e63c74bcabc34e89423e883d
Mitroshin <mitroshin.develop@gmail.com>
2017-02-25 10:31:04 +0300

    Добавлена зависимость от библиотеки поддержки.

--- app/src/main/java/com/developgmail/mitroshin/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/developgmail/mitroshin/criminalintent/CrimeActivity.java
@@ -1,13 +1,13 @@
 package com.developgmail.mitroshin.criminalintent;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class CrimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_crime);
     }
 }
