9f17219efe715a0e4754798a915e9699789e7842
Mitroshin <mitroshin.develop@gmail.com>
2017-02-27 23:51:44 +0300

    Обновлен уровень контроллера: добавлена пустая реализация фрагмента списка.

---
+++ app/src/main/java/com/developgmail/mitroshin/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.developgmail.mitroshin.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
