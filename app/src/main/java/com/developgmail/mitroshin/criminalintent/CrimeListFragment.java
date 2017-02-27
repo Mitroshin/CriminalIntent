3e3bbc0371b07892caa6c2b7e7cf69f66ea30e72
Mitroshin <mitroshin.develop@gmail.com>
2017-02-28 00:55:58 +0300

    Реализовано отображение элементов списка в виде обычных строк.

--- app/src/main/java/com/developgmail/mitroshin/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/developgmail/mitroshin/criminalintent/CrimeListFragment.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class CrimeListFragment extends Fragment {
 
     private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
 
     @Nullable
     @Override
@@ -25,7 +26,16 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUI();
+
         return view;
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder {
