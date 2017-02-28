40f695ad7a9d201756577229603798b4bf725992
Mitroshin <mitroshin.develop@gmail.com>
2017-02-28 12:20:23 +0300

    Добавлена возможность запуска представления детализация из списка.

--- app/src/main/java/com/developgmail/mitroshin/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/developgmail/mitroshin/criminalintent/CrimeActivity.java
@@ -1,11 +1,24 @@
 package com.developgmail.mitroshin.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
+
+    public static final String EXTRA_CRIME_ID =
+            "com.developgmail.mitroshin.criminalintent.crime_id";
 
     @Override
     protected Fragment createFragment() {
         return new CrimeFragment();
+    }
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
     }
 }
