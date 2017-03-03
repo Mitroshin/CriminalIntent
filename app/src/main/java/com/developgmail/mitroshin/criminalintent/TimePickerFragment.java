d75bd52e2660fd7a0172709741e8aad816fcc47d
Mitroshin <mitroshin.develop@gmail.com>
2017-03-03 19:08:01 +0300

    Обновлен макет диалогового окна для выбора времени преступления

--- app/src/main/java/com/developgmail/mitroshin/criminalintent/TimePickerFragment.java
+++ app/src/main/java/com/developgmail/mitroshin/criminalintent/TimePickerFragment.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class TimePickerFragment extends DialogFragment {
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_time, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.time_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
