20b2f4f233d7ce533f401a64603657857fdabe12
Mitroshin <mitroshin.develop@gmail.com>
2017-03-06 15:59:12 +0300

    Доработан метод для чтения данных из курсорв.

--- app/src/main/java/com/developgmail/mitroshin/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/developgmail/mitroshin/criminalintent/database/CrimeCursorWrapper.java
@@ -6,6 +6,9 @@ import android.database.CursorWrapper;
 import com.developgmail.mitroshin.criminalintent.Crime;
 import com.developgmail.mitroshin.criminalintent.database.CrimeDBSchema.CrimeTable;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class CrimeCursorWrapper extends CursorWrapper {
     public CrimeCursorWrapper(Cursor cursor) {
         super(cursor);
@@ -17,6 +20,11 @@ public class CrimeCursorWrapper extends CursorWrapper {
         long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
         int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
 
-        return null;
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+
+        return crime;
     }
 }
