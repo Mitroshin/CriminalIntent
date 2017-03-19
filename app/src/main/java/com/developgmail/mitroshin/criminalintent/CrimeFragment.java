f4f53c3675908dcd5a9e9d0f9511637b19cbe621
Mitroshin <mitroshin.develop@gmail.com>
2017-03-19 22:04:29 +0300

    Добавлены методы для получения файла фотографии.

--- app/src/main/java/com/developgmail/mitroshin/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/developgmail/mitroshin/criminalintent/CrimeFragment.java
@@ -28,6 +28,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.Date;
@@ -52,6 +53,8 @@ public class CrimeFragment extends Fragment{
     private Button mReportButton;
     private Button mSuspectButton;
     private ImageButton mCallButton;
+    private ImageButton mPhotoButton;
+    private ImageView mPhotoView;
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -254,6 +257,9 @@ public class CrimeFragment extends Fragment{
         if (packageManager.resolveActivity(pickContact, PackageManager.MATCH_DEFAULT_ONLY) == null) {
             mSuspectButton.setEnabled(false);
         }
+
+        mPhotoButton = (ImageButton) view.findViewById(R.id.crime_camera);
+        mPhotoView = (ImageView) view.findViewById(R.id.crime_photo);
 
         return view;
     }
