4cdf8680be5fd51b7ad760b6e608fddba111bded
Mitroshin <mitroshin.develop@gmail.com>
2017-03-19 23:08:12 +0300

    Добавлен метод для масштабирования изображения.

---
+++ app/src/main/java/com/developgmail/mitroshin/criminalintent/PicturesUtil.java
@@ -0,0 +1,29 @@
+package com.developgmail.mitroshin.criminalintent;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class PicturesUtil {
+    public static Bitmap getScaledBitmap(String path, int desWidth, int destHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        int inSampleSize = 1;
+        if (srcHeight > destHeight || srcWidth > desWidth) {
+            if (srcWidth > srcHeight) {
+                inSampleSize = Math.round(srcHeight / destHeight);
+            } else {
+                inSampleSize = Math.round(srcWidth / desWidth);
+            }
+        }
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+
+        return BitmapFactory.decodeFile(path, options);
+    }
+}
